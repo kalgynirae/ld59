a00669fde63f85bb6ed9fb09d8fe19b2a0f62dd6
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2026-04-18 11:29:26 -0700

    feat: Added data to the tilemaps

--- scenes/map.gd
+++ scenes/map.gd
@@ -1,5 +1,19 @@
 extends Node2D
 
+enum Objects {
+	PLANT = 1
+}
+
+func is_touching(obj: Objects) -> bool:
+	var head_pos = $Snake.gridlocs[0]
+	
+	var data = $Ground/Decorations.get_cell_tile_data(head_pos)
+	
+	if data != null:
+		return data.get_custom_data("interact") == obj
+	
+	return false
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_right"):
 		$Snake.move("right")
