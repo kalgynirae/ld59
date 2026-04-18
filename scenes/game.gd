495392b3c7af8339037da400ec1d889106d5f65d
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2026-04-18 12:23:34 -0700

    feat: Switching between four levels

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,23 +3,45 @@ extends Node2D
 const VIEWPORT_WIDTH: int = 192
 const VIEWPORT_HEIGHT: int = 144
 
+const TILE_WIDTH: int = 24
+const TILE_HEIGHT: int = 18
+
 var current_map: Maps = Maps.Start
 
 enum Maps {
 	Start,
-	Dessert
+	Dessert,
+	Rain,
+	Switch,
+	Box
 }
 
 func _process(delta: float) -> void:
 	var snake_pos: Vector2i = $Map/Snake.gridlocs[0]
 	
-	if snake_pos.x >= 0 && snake_pos.x < VIEWPORT_WIDTH:
+	if snake_pos.y >= -TILE_HEIGHT && snake_pos.y < 0:
+		current_map = Maps.Switch
+	elif snake_pos.y >= TILE_HEIGHT && snake_pos.y < TILE_HEIGHT * 2:
+		current_map = Maps.Box
+	elif snake_pos.x >= 0 && snake_pos.x < TILE_WIDTH:
 		current_map = Maps.Start
-	if snake_pos.x >= -VIEWPORT_WIDTH && snake_pos.x < 0:
+	elif snake_pos.x >= -TILE_WIDTH && snake_pos.x < 0:
 		current_map = Maps.Dessert
+	elif snake_pos.x >= TILE_WIDTH && snake_pos.x < TILE_WIDTH * 2:
+		current_map = Maps.Rain
 	
+	print("Current map")
+	print(current_map)
+	print(snake_pos)
+
 	match current_map:
 		Maps.Start:
 			$Camera.offset = Vector2i(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
 		Maps.Dessert:
 			$Camera.offset = Vector2i(-VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
+		Maps.Rain:
+			$Camera.offset = Vector2i(VIEWPORT_WIDTH * 3, VIEWPORT_HEIGHT)
+		Maps.Switch:
+			$Camera.offset = Vector2i(VIEWPORT_WIDTH, -VIEWPORT_HEIGHT)
+		Maps.Box:
+			$Camera.offset = Vector2i(VIEWPORT_WIDTH, VIEWPORT_HEIGHT * 3)
