e12bb6bd307558eeff119c2fada5800ecb2502bc
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2026-04-18 11:51:57 -0700

    feat: Added second map

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,4 +1,26 @@
 extends Node2D
 
+const VIEWPORT_WIDTH: int = 192
+const VIEWPORT_HEIGHT: int = 144
+
+var current_map: Maps = Maps.Start
+
+enum Maps {
+	Start,
+	Dessert
+}
+
 func _process(delta: float) -> void:
-	pass
+	var snake_pos: Vector2i = $Map/Snake.gridlocs[0]
+	
+	print(snake_pos)
+	if snake_pos.x > 0 && snake_pos.x < VIEWPORT_WIDTH:
+		current_map = Maps.Start
+	if snake_pos.x > -VIEWPORT_WIDTH && snake_pos.x < 0:
+		current_map = Maps.Dessert
+	
+	match current_map:
+		Maps.Start:
+			$Camera.offset = Vector2i(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
+		Maps.Dessert:
+			$Camera.offset = Vector2i(-VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
