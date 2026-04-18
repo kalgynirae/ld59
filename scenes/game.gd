ee79db35eabb2c6aa7702541c93f6e11d0c3155b
Colin Chan <colin+git@lumeh.org>
2026-04-18 15:48:42 -0700

    Resize screens (20x16) and fix camera logic

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-const VIEWPORT_WIDTH: int = 192
-const VIEWPORT_HEIGHT: int = 144
+const VIEWPORT_WIDTH: int = 320
+const VIEWPORT_HEIGHT: int = 256
 
-const TILE_WIDTH: int = 24
-const TILE_HEIGHT: int = 18
+const TILE_WIDTH: int = 20
+const TILE_HEIGHT: int = 16
 
 var current_map: Maps = Maps.Start
 
 enum Maps {
 	Start,
-	Dessert,
+	Desert,
 	Rain,
 	Switch,
 	Box
@@ -18,7 +18,7 @@ enum Maps {
 
 func _process(delta: float) -> void:
 	var snake_pos: Vector2i = $Map/Snake.gridlocs[0]
-	
+
 	if snake_pos.y >= -TILE_HEIGHT && snake_pos.y < 0:
 		current_map = Maps.Switch
 	elif snake_pos.y >= TILE_HEIGHT && snake_pos.y < TILE_HEIGHT * 2:
@@ -26,18 +26,18 @@ func _process(delta: float) -> void:
 	elif snake_pos.x >= 0 && snake_pos.x < TILE_WIDTH:
 		current_map = Maps.Start
 	elif snake_pos.x >= -TILE_WIDTH && snake_pos.x < 0:
-		current_map = Maps.Dessert
+		current_map = Maps.Desert
 	elif snake_pos.x >= TILE_WIDTH && snake_pos.x < TILE_WIDTH * 2:
 		current_map = Maps.Rain
 
 	match current_map:
 		Maps.Start:
-			$Camera.offset = Vector2i(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
-		Maps.Dessert:
-			$Camera.offset = Vector2i(-VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
+			$Camera.position = Vector2i(0, 0)
+		Maps.Desert:
+			$Camera.position = Vector2i(-VIEWPORT_WIDTH, 0)
 		Maps.Rain:
-			$Camera.offset = Vector2i(VIEWPORT_WIDTH * 3, VIEWPORT_HEIGHT)
+			$Camera.position = Vector2i(VIEWPORT_WIDTH, 0)
 		Maps.Switch:
-			$Camera.offset = Vector2i(VIEWPORT_WIDTH, -VIEWPORT_HEIGHT)
+			$Camera.position = Vector2i(0, -VIEWPORT_HEIGHT)
 		Maps.Box:
-			$Camera.offset = Vector2i(VIEWPORT_WIDTH, VIEWPORT_HEIGHT * 3)
+			$Camera.position = Vector2i(0, VIEWPORT_HEIGHT)
